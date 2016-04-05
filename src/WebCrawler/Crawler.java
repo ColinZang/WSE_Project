288d14f7a50b99ab48417ad5665f3fab49033bf0
Wenzhao Zang <zangwz@gmail.com>
2016-04-04 22:33:33 -0400

    test 3

--- src/WebCrawler/Crawler.java
+++ src/WebCrawler/Crawler.java
@@ -5,6 +5,6 @@ package WebCrawler;
  */
 public class Crawler {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to web search engine project");
     }
 }
