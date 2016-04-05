260327d60e3d1bb06bafbdfcfb4751f797958f97
Min Guo <xmguomin@gmail.com>
2016-04-05 14:38:25 -0400

    Update

--- src/WebCrawler/Crawler.java
+++ src/WebCrawler/Crawler.java
@@ -5,11 +5,6 @@ package WebCrawler;
  */
 public class Crawler {
     public static void main(String[] args) {
-        System.out.println("Welcome to web search engine project");
-        System.out.println("WebSearchEngine");
-        System.out.println("Welcome to web search engine project!!!");
-        System.out.println("welcome");
-        System.out.println("Change by Min");
-        System.out.println("web");
+
     }
 }
