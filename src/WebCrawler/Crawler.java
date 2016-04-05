84d8aa734bc04a199e305652814ce5e746c873d9
libinjungle <libinnku@gmail.com>
2016-04-05 13:59:39 -0400

    Bin's commit

--- src/WebCrawler/Crawler.java
+++ src/WebCrawler/Crawler.java
@@ -1,9 +1,16 @@
 package WebCrawler;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Min on 4/4/16.
  * test by Bin
  */
-public class Crawler {
 
+public class Crawler {
+    public static void main(String[] args) {
+    }
 }
