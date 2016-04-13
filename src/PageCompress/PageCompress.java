21625eaaffcdae74c3f8c4c597da8b16536a65fc
bzsmz1990 <bzsmz1990@gmail.com>
2016-04-13 18:19:11 -0400

    Add PageCompress function into Crawler

--- src/PageCompress/PageCompress.java
+++ src/PageCompress/PageCompress.java
@@ -9,100 +9,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 
 public class PageCompress {
-    private String dataPath;
-    private String resultPath;
+    private String pageID;  // file name
+    private String pageHTML;    // page content (tag+text)
 
-    PageCompress(String dp, String rp) {
-        dataPath = dp;
-        resultPath = rp;
-    }
-
-    private void run() {
-        File dataDir = null;
-        File resultDir = null;
-        try {
-            dataDir = new File(dataPath);
-            resultDir = new File(resultPath);
-        } catch (Exception e) {
-            System.out.println("data path or result path is not directory!");
-            System.exit(1);
-        }
-
-        File[] files = dataDir.listFiles();
-        for (File file : files) {
-            String name = file.getName();
-            String ext = getExtension(name);
-            if (!ext.equals("html")) {
-                System.out.println("File \"" + name + "\" is not a web page");
-                continue;
-            }
-            String nameWoExt = name.substring(0, name.length() - 5);
-            String pageHTML = readPage(file);
-            pageHTML = MakeupPageHTML(pageHTML);
-            PageFile pageFile = GetPage(nameWoExt, pageHTML);
-            SavePageFile(pageFile, resultDir);
-        }
-    }
-
-    /*
-     * get file extension
-     */
-    private String getExtension(String filename) {
-        if (filename == null) {
-            return null;
-        }
-        int extensionPos = filename.lastIndexOf('.');
-        int lastUnixPos = filename.lastIndexOf('/');
-        int lastWindowsPos = filename.lastIndexOf('\\');
-        int lastSeparator = Math.max(lastUnixPos, lastWindowsPos);
-
-        int index = lastSeparator > extensionPos ? -1 : extensionPos;
-        if (index == -1) {
-            return "";
-        } else {
-            return filename.substring(index + 1);
-        }
-    }
-
-    /*
-     * read pages from file
-     */
-    private String readPage(File file) {
-        String wholePage = "";
-        try {
-            InputStream stream = new FileInputStream(file);
-            byte[] tempbytes = new byte[1000];
-            int byteread = 0;
-            while ((byteread = stream.read(tempbytes)) != -1) {
-                String newContent = new String(tempbytes, 0, byteread);
-                wholePage += newContent;
-            }
-            stream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            return wholePage;
-        }
-    }
-
-    /*
-     * manually modify some special tag case
-     */
-    private String MakeupPageHTML(String pageHTML) {
-        // tricky case1: for some tag, such like <option>, if there is not seperator in text
-        // HTML parser will make spaceless concatenation problem
-        // this is not the fault of Jsoup, it's the falut of UI designer
-        // (we should know this method is not stable, if we meet more special case,
-        // we need add them into here)
-        pageHTML = pageHTML.replaceAll("</option>", " </option>");
-        return pageHTML;
+    public PageCompress(String name, String page) {
+        pageID = name;
+        pageHTML = page;
     }
 
     /*
@@ -111,14 +29,24 @@ public class PageCompress {
      * and it can filter <script> in <body> as well while "Jtidy"
      * cannot do it
      */
-    private PageFile GetPage(String nameWoExt, String wholePage) {
-        Document doc = Jsoup.parse(wholePage);
+    public PageFile GetPageFile() throws Exception {
 
+        if (pageID == null || pageID.equals("")) {
+            throw new Exception("pageID cannot be empty!");
+        }
+
+        // manually modify some special tag case
+        pageHTML = MakeupPageHTML(pageHTML);
+
+        Document doc = Jsoup.parse(pageHTML);
+
+        // get page title
         String title = doc.title();
         if (title == null || title.equals("")) {
             title = "NOTITLE";
         }
 
+        // get page body text content
         String bodyText = doc.body().text();
 
         // we can filter duplicated subURL at here
@@ -134,47 +62,20 @@ public class PageCompress {
         }
         List<String> uniqueSubURLs = new ArrayList<>(subURLs);
 
-        PageFile pageFile = new PageFile(nameWoExt, title, uniqueSubURLs, bodyText);
+        PageFile pageFile = new PageFile(pageID, title, uniqueSubURLs, bodyText);
         return pageFile;
     }
 
     /*
-     * save compressed page content into file
+     * manually modify some special tag case
      */
-    private void SavePageFile(PageFile pf, File dir) {
-        String path = dir.getAbsolutePath() + File.separator + pf.PageID;
-        File file = new File(path);
-        try (FileOutputStream fos = new FileOutputStream(file)){
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            String fileContent = "#TITLE#\n" + pf.title + "\n\n" + "#SUBURLS#\n";
-            for (String url : pf.subURLs) {
-                fileContent += url + "\n";
-            }
-            fileContent += "\n#CONTENT#\n";
-            fileContent += pf.content;
-
-            byte[] contentInBytes = fileContent.getBytes();
-
-            fos.write(contentInBytes);
-            fos.flush();
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("Save page: " + pf.PageID);
-    }
-
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("You should give pathes for data and result folder");
-            System.exit(1);
-        }
-
-        String dataPath = args[0];
-        String resultPath = args[1];
-        PageCompress pc = new PageCompress(dataPath, resultPath);
-        pc.run();
+    private String MakeupPageHTML(String pageHTML) {
+        // tricky case1: for some tag, such like <option>, if there is not seperator in text
+        // HTML parser will make spaceless concatenation problem
+        // this is not the fault of Jsoup, it's the falut of UI designer
+        // (we should know this method is not stable, if we meet more special case,
+        // we need add them into here)
+        pageHTML = pageHTML.replaceAll("</option>", " </option>");
+        return pageHTML;
     }
 }
--- src/PageCompress/PageFile.java
+++ src/PageCompress/PageFile.java
@@ -7,19 +7,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PageFile {
-    String PageID;
-    String title;
-    List<String> subURLs;
-    String content;
+    private String pageID;
+    private String title;
+    private List<String> subURLs;
+    private String content;
 
-    PageFile() {
+    public PageFile() {
         subURLs = new ArrayList<String>();
     }
 
-    PageFile(String id, String t, List<String> urls, String c) {
-        PageID = id;
+    public PageFile(String id, String t, List<String> urls, String c) {
+        pageID = id;
         title = t;
         subURLs = urls;
         content = c;
+    }
+
+    public String getPageID() {
+        return pageID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public List<String> getSubURLs() {
+        return subURLs;
     }
 }
